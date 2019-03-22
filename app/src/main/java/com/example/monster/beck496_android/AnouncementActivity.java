ea8b02b6f9dd93147be2264e05c811bc991cc19d
emeldinc <emeldincc@gmail.com>
2019-03-22 22:27:49 +0300

    Add duyuru ekle page in announcement activity

--- app/src/main/java/com/example/monster/beck496_android/AnouncementActivity.java
+++ app/src/main/java/com/example/monster/beck496_android/AnouncementActivity.java
@@ -3,26 +3,40 @@ package com.example.monster.beck496_android;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AnouncementActivity extends AppCompatActivity {
+    String baslik, aciklama;
+    EditText baslikInput;
+    EditText aciklamaInput;
+    Button kaydetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anouncement);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        baslikInput = (EditText) findViewById(R.id.baslikInput);
+        aciklamaInput = (EditText) findViewById(R.id.aciklamaInput);
+        kaydetButton = (Button) findViewById(R.id.kaydetButton);
+        kaydetButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                baslik = baslikInput.getText().toString();
+                aciklama = aciklamaInput.getText().toString();
+                String text = baslik + "\n"+ aciklama;
+                showToast(text);
+
             }
+
         });
+    }
+    private void showToast(String message){
+        Toast.makeText(AnouncementActivity.this, message, Toast.LENGTH_SHORT).show();
     }
 }
